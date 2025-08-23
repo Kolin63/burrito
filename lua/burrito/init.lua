618cf4e6a02249a99bbd6c587c85272ac974c112
Kolin63 <theverylastgoodname@gmail.com>
2025-08-23 03:09:48 -0400

    Add Basic Line Wrapping

--- lua/burrito/init.lua
+++ lua/burrito/init.lua
@@ -1,23 +1,33 @@
-print("burrito")
+function burrito_check()
+  -- get all lines in buffer
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
+  -- needs to be a variable so we can change it
+  local lines_amt = #lines
+
+  for i = 1, lines_amt do
+    local line = lines[i]
+
+    -- check line length
+    if #line > 80 then
+
+      local new_lines = {}
+      repeat
+        table.insert(new_lines, line:sub(1, 80))
+        line = line:sub(81)
+        lines_amt = lines_amt + 1
+      until #line == 0
+
+      vim.api.nvim_buf_set_lines(0, i-1, i, false, new_lines)
+      local old_cursor_pos = vim.api.nvim_win_get_cursor(0)
+      vim.api.nvim_win_set_cursor(0, { old_cursor_pos[1] + #new_lines - 1, 1 })
+    end
+  end
+end
+
+-- set the auto command to check the file for lines that need to be wrapped
+-- whenever the file is changed
 vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
   pattern = "*.md",
-  callback = function()
-    print("changed")
-  end
+  callback = burrito_check
 })
-
-[[
-for each line in file:
-  if length of line > 80:
-    bool changed
-    for col = 80; col > 0; --col:
-      if line[col] == space or hyphon:
-        changed = true
-        insert newline
-        break
-    if !changed:
-      insert newline at col 80
-    if line[i+2] doesn't start with space or asterisk or hyphon:
-      join next line and line i+2
-]]
